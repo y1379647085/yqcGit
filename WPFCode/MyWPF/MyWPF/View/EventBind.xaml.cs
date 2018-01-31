e6916e65a02b7ab38ee258dc34cb49ac060ecbc0
y1379647085 <1379647085@qq.com>
2018-01-31 18:14:49 +0800

    WPF增加事件绑定

---
+++ WPFCode/MyWPF/MyWPF/View/EventBind.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyWPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyWPF.View
+{
+    /// <summary>
+    /// EventBind.xaml 的交互逻辑
+    /// </summary>
+    public partial class EventBind : Window
+    {
+        public EventBind()
+        {
+            InitializeComponent();
+            DataContext = new EventBindViewModel();
+        }
+    }
+}
